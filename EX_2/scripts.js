b631e9430b0093ac31421dd668946b2f2a5198df
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-09 13:26:12 +0200

    Refactor

--- EX_2/scripts.js
+++ EX_2/scripts.js
@@ -16,36 +16,20 @@ $.ajax({
 });
 
 let updateJSONbin = function () {
-    $.ajax({
-        url: 'https://api.jsonbin.io/b/5f7e132565b18913fc5c3ad5',
-        type: 'PUT',
-        headers: {
-            'secret-key': '$2b$10$IV.mOVlzxoKQ8My.jMcEJO1.q8w7dn.DQ2rF85vjyyxScHLV.XuOe'
-        },
-        contentType: 'application/json',
-        data: JSON.stringify(todoList),
-        success: (data) => {
-            console.log(data);
-        },
-        error: (err) => {
-            console.log(err.responseJSON);
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+        if (req.readyState === XMLHttpRequest.DONE) {
+            console.log(req.responseText);
         }
-    });
-    // let req = new XMLHttpRequest();
-    //
-    // req.onreadystatechange = () => {
-    //     if (req.readyState === XMLHttpRequest.DONE) {
-    //         console.log(req.responseText);
-    //     }
-    // };
-    //
-    // req.open("PUT", "https://api.jsonbin.io/b/5f7e132565b18913fc5c3ad5", true);
-    // req.setRequestHeader("Content-Type", "application/json");
-    // req.setRequestHeader("secret-key", '$2b$10$IV.mOVlzxoKQ8My.jMcEJO1.q8w7dn.DQ2rF85vjyyxScHLV.XuOe');
-    // req.send(todoList);
+    };
+
+    req.open("PUT", "https://api.jsonbin.io/b/5f7e132565b18913fc5c3ad5", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("secret-key", '$2b$10$IV.mOVlzxoKQ8My.jMcEJO1.q8w7dn.DQ2rF85vjyyxScHLV.XuOe');
+    req.send(todoList);
 }
 
-//TODO: dodawane elementy w tablicy są dodane prawidłowo, natomiast w tabelii pokazują się elementy w odwrotnej kolejności
 let updateTodoList = function () {
     let todoListDiv = $('#todoListView')[0];
 
@@ -57,22 +41,22 @@ let updateTodoList = function () {
     let filterInputFirstDate = $('#inputSearchFirstDate')[0];
     let filterInputLastDate = $('#inputSearchLastDate')[0];
 
-    let newElement = $('<table id="todoTable"/>').addClass("table table-striped table-bordered").appendTo(todoListDiv);
-    let tHead = $('<thead></thead>').addClass("thead-dark").appendTo(newElement);
+    let table = $('<table id="todoTable"/>').addClass("table table-striped table-bordered").appendTo(todoListDiv);
+    let tHead = $('<thead></thead>').addClass("thead-dark").appendTo(table);
     let row = $('<tr></tr>').appendTo(tHead);
 
     let text = Object.keys(todoList[0]).find(key => todoList[0][key] === todoList[0].title);
-    let $th_title = $('<th></th>').appendTo(row).text(text);
-    $th_title.scope = "col";
+    let $thTitle = $('<th></th>').appendTo(row).text(text);
+    $thTitle.scope = "col";
 
     text = Object.keys(todoList[0]).find(key => todoList[0][key] === todoList[0].description);
-    let th_desc = $('<th></th>').appendTo(row).text(text);
-    th_desc.className = "col-sm-2";
+    let thDescription = $('<th></th>').appendTo(row).text(text);
+    thDescription.className = "col-sm-2";
 
-    let th_del = $('<th></th>').appendTo(row).text("delete");
-    th_del.className = "col-xs-4";
+    let thDelete = $('<th></th>').appendTo(row).text("delete");
+    thDelete.className = "col-xs-4";
 
-    let tBody = $('<tbody></tbody>').appendTo(newElement);
+    let tBody = $('<tbody></tbody>').appendTo(table);
     for (let element of todoList) {
         //TODO: nowe elementy nie działaja na wyszukiwanie data
         if ((filterInputTitle.value === "" ||
@@ -88,8 +72,8 @@ let updateTodoList = function () {
             $('<td></td>').appendTo(row).addClass("col-xs-4").text(element.title);
             $('<td></td>').appendTo(row).addClass("col-sm-2").text(element.description);
 
-            let td_del = $('<td></td>').appendTo(row).addClass("col-xs-4");
-            let newDeleteButton = $('<input type="button" value="x"/>').appendTo(row).addClass("btn btn-dark").appendTo(td_del);
+            let tdDelete = $('<td></td>').appendTo(row).addClass("col-xs-4");
+            let newDeleteButton = $('<input type="button" value="x"/>').appendTo(row).addClass("btn btn-dark").appendTo(tdDelete);
             newDeleteButton.click(function () {
                 deleteTodo(todoList.indexOf(element));
             });
