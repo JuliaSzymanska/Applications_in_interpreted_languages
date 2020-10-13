4b559b5359951e45d2dd28d8b6b998e043c3cd3c
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-13 20:47:46 +0200

    Added two components

--- EX_3/baza-filmow/src/App.vue
+++ EX_3/baza-filmow/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Forms/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Forms from './components/Forms'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Forms
   }
 }
 </script>
---
+++ EX_3/baza-filmow/src/components/Forms.vue
@@ -0,0 +1,36 @@
+<template>
+  <form id="forms">
+    <div class="col-xs">
+      <label for="inputTitle">Tytuł: </label>
+      <input v-model="inputTitle" id="inputTitle" class="form-control"
+             placeholder="Podaj tytuł lub fragment tytułu filmu">
+      <p>Message: {{ inputTitle }}</p>
+    </div>
+    <div class="col-xm">
+      <label for="inputYearFrom">Rok produkcji od: </label>
+      <input v-model="inputYearFrom" id="inputYearFrom" class="form-control"
+             placeholder="Liczba naturalna z przedziału 1900-2019">
+    </div>
+    <div class="col-xs">
+      <label for="inputYearTo">Rok produkcji do: </label>
+      <input v-model="inputYearTo" id="inputYearTo" class="form-control"
+             placeholder="Liczba naturalna z przedziału 1900-2019"/>
+    </div>
+    <div class="col-xs">
+      <label for="inputCast">Obsada: </label>
+      <input v-model="inputCast" id="inputCast" class="form-control" placeholder="Imię i nazwisko"/>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Forms"
+}
+</script>
+
+<style scoped>
+#forms {
+  padding: 20px;
+}
+</style>
