9a0da219db0549593cd443b3e956beaaffea351b
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-23 13:16:26 +0200

    Working List genres

--- EX_3/baza-filmow/src/components/ListGenres.vue
+++ EX_3/baza-filmow/src/components/ListGenres.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="listGenres">
     <div class="col-md">
-    <h2>Filmy wg gatunku</h2>
+      <h2>Filmy wg gatunku</h2>
+<!-- TODO: pewnie można to jakoś ładnie zrobić w JS za pomocą fora czy cos -->
+      <h3 style="margin-top: 30px">{{ this.genresArray[0] }}</h3>
       <ol>
-        <li v-for="film in this.searchGenres()" v-bind:key="film.title">{{ film.title }}</li>
+        <li v-for="film in this.searchGenres(this.genresArray[0])" v-bind:key="film.title">{{ film.title }}</li>
+      </ol>
+      <h3 style="margin-top: 30px">{{ this.genresArray[1] }}</h3>
+      <ol>
+        <li v-for="film in this.searchGenres(this.genresArray[1])" v-bind:key="film.title">{{ film.title }}</li>
+      </ol>
+      <h3 style="margin-top: 30px">{{ this.genresArray[2] }}</h3>
+      <ol>
+        <li v-for="film in this.searchGenres(this.genresArray[2])" v-bind:key="film.title">{{ film.title }}</li>
       </ol>
     </div>
   </div>
@@ -19,21 +29,28 @@ export default {
   data() {
     return {
       Films: films,
-      n: 10
+      n: 10,
+      genresArray: ["Comedy", "Documentary", "Short",]
     }
   },
-  computed: {},
+  computed: {
+
+    createList: function () {
+
+    }
+
+  },
   methods: {
-    searchGenres: function () {
+    searchGenres: function (genre) {
       return _.filter(this.Films, function (film) {
         let isInIt = false
         for (let i = 0; i < film.cast.length; i++) {
-            if (film.genres[i] === "Comedy") {
-              isInIt = true
-            }
+          if (film.genres[i] === genre) {
+            isInIt = true
+          }
         }
         return isInIt
-        })
+      }).slice(0, 10)
     },
   }
 }
