870701f70bcec0b8fb3f7e43d695d5ba0bcaf506
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-23 13:32:04 +0200

    Cast list

--- EX_3/baza-filmow/src/components/ListCast.vue
+++ EX_3/baza-filmow/src/components/ListCast.vue
@@ -1,12 +1,52 @@
 <template>
-  <div>
-<!--TODO: trzbea skopiować będzie z ListGenres-->
+  <div id="listCast">
+    <div class="col-md">
+      <h2>Filmy wg obsady</h2>
+<!--      &lt;!&ndash; TODO: pewnie można to jakoś ładnie zrobić w JS za pomocą fora czy cos &ndash;&gt;-->
+      <h3 style="margin-top: 30px">{{ this.castArray[0] }}</h3>
+      <ol>
+        <li v-for="film in this.searchGenres(this.castArray[0])" v-bind:key="film.title">{{ film.title }}</li>
+      </ol>
+      <h3 style="margin-top: 30px">{{ this.castArray[1] }}</h3>
+      <ol>
+        <li v-for="film in this.searchGenres(this.castArray[1])" v-bind:key="film.title">{{ film.title }}</li>
+      </ol>
+      <h3 style="margin-top: 30px">{{ this.castArray[2] }}</h3>
+      <ol>
+        <li v-for="film in this.searchGenres(this.castArray[2])" v-bind:key="film.title">{{ film.title }}</li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
+
+import films from '../films'
+import _ from 'lodash';
+
 export default {
-  name: "ListCast"
+  name: "ListCast",
+  data() {
+    return {
+      Films: films,
+      n: 10,
+      castArray: ["Paul Boyton", "Florence Lawrence", "George Gebhardt",]
+    }
+  },
+  computed: {},
+  methods: {
+    searchGenres: function (cast) {
+      return _.filter(this.Films, function (film) {
+        let isInIt = false
+        for (let i = 0; i < film.cast.length; i++) {
+          if (film.cast[i] === cast) {
+            isInIt = true
+          }
+        }
+        return isInIt
+      }).slice(0, 10)
+    },
+  }
 }
 </script>
 
--- EX_3/baza-filmow/src/components/ListGenres.vue
+++ EX_3/baza-filmow/src/components/ListGenres.vue
@@ -2,19 +2,19 @@
   <div id="listGenres">
     <div class="col-md">
       <h2>Filmy wg gatunku</h2>
-<!-- TODO: pewnie można to jakoś ładnie zrobić w JS za pomocą fora czy cos -->
-      <h3 style="margin-top: 30px">{{ this.genresArray[0] }}</h3>
-      <ol>
-        <li v-for="film in this.searchGenres(this.genresArray[0])" v-bind:key="film.title">{{ film.title }}</li>
-      </ol>
-      <h3 style="margin-top: 30px">{{ this.genresArray[1] }}</h3>
-      <ol>
-        <li v-for="film in this.searchGenres(this.genresArray[1])" v-bind:key="film.title">{{ film.title }}</li>
-      </ol>
-      <h3 style="margin-top: 30px">{{ this.genresArray[2] }}</h3>
-      <ol>
-        <li v-for="film in this.searchGenres(this.genresArray[2])" v-bind:key="film.title">{{ film.title }}</li>
-      </ol>
+        <!-- TODO: pewnie można to jakoś ładnie zrobić w JS za pomocą fora czy cos -->
+        <h3 style="margin-top: 30px">{{ this.genresArray[0] }}</h3>
+        <ol>
+          <li v-for="film in this.searchGenres(this.genresArray[0])" v-bind:key="film.title">{{ film.title }}</li>
+        </ol>
+        <h3 style="margin-top: 30px">{{ this.genresArray[1] }}</h3>
+        <ol>
+          <li v-for="film in this.searchGenres(this.genresArray[1])" v-bind:key="film.title">{{ film.title }}</li>
+        </ol>
+        <h3 style="margin-top: 30px">{{ this.genresArray[2] }}</h3>
+        <ol>
+          <li v-for="film in this.searchGenres(this.genresArray[2])" v-bind:key="film.title">{{ film.title }}</li>
+        </ol>
     </div>
   </div>
 </template>
@@ -33,18 +33,12 @@ export default {
       genresArray: ["Comedy", "Documentary", "Short",]
     }
   },
-  computed: {
-
-    createList: function () {
-
-    }
-
-  },
+  computed: {},
   methods: {
     searchGenres: function (genre) {
       return _.filter(this.Films, function (film) {
         let isInIt = false
-        for (let i = 0; i < film.cast.length; i++) {
+        for (let i = 0; i < film.genres.length; i++) {
           if (film.genres[i] === genre) {
             isInIt = true
           }
