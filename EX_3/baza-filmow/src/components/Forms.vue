d4c355a0dfaa50fe9d6184a87c351a6983e2c3ad
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-24 19:35:11 +0100

    Deleted comments

--- EX_3/baza-filmow/src/components/Forms.vue
+++ EX_3/baza-filmow/src/components/Forms.vue
@@ -4,34 +4,34 @@
       <label for="inputTitle" class="col-sm-2 col-form-label">Tytuł: </label>
       <div class="col-sm-10">
         <input
-          v-model="inputTitle"
-          id="inputTitle"
-          class="form-control"
-          placeholder="Podaj tytuł lub fragment tytułu filmu"
+            v-model="inputTitle"
+            id="inputTitle"
+            class="form-control"
+            placeholder="Podaj tytuł lub fragment tytułu filmu"
         />
       </div>
     </div>
     <div class="form-group row">
       <label for="inputYearFrom" class="col-sm-2 col-form-label"
-        >Rok produkcji od:
+      >Rok produkcji od:
       </label>
       <div class="col-sm-4">
         <input
-          v-model="inputYearFrom"
-          id="inputYearFrom"
-          class="form-control"
-          placeholder="Liczba naturalna z przedziału 1900-2019"
+            v-model="inputYearFrom"
+            id="inputYearFrom"
+            class="form-control"
+            placeholder="Liczba naturalna z przedziału 1900-2019"
         />
       </div>
       <label for="inputYearTo" class="col-sm-2 col-form-label"
-        >Rok produkcji do:
+      >Rok produkcji do:
       </label>
       <div class="col-sm-4">
         <input
-          v-model="inputYearTo"
-          id="inputYearTo"
-          class="form-control"
-          placeholder="Liczba naturalna z przedziału 1900-2019"
+            v-model="inputYearTo"
+            id="inputYearTo"
+            class="form-control"
+            placeholder="Liczba naturalna z przedziału 1900-2019"
         />
       </div>
     </div>
@@ -39,18 +39,18 @@
       <label for="inputCast" class="col-sm-2 col-form-label">Obsada: </label>
       <div class="col-sm-10">
         <input
-          v-model="inputCast"
-          id="inputCast"
-          class="form-control"
-          placeholder="Imię i nazwisko"
+            v-model="inputCast"
+            id="inputCast"
+            class="form-control"
+            placeholder="Imię i nazwisko"
         />
       </div>
     </div>
     <div>
       <button
-        type="button"
-        class="btn btn-primary btn-lg btn-block"
-        v-on:click="search"
+          type="button"
+          class="btn btn-primary btn-lg btn-block"
+          v-on:click="search"
       >
         Szukaj
       </button>
@@ -61,6 +61,7 @@
 <script>
 import films from "../films";
 import _ from "lodash";
+
 export default {
   name: "Forms",
   data: function () {
@@ -78,10 +79,10 @@ export default {
       let self = this;
       this.listEmitted = _.filter(this.Films, function (film) {
         if (
-          (self.inputTitle === "" ||
-            film.title.toLowerCase().includes(self.inputTitle.toLowerCase())) &&
-          (film.year >= self.inputYearFrom || self.inputYearFrom === "") &&
-          (film.year <= self.inputYearTo || self.inputYearTo === "")
+            (self.inputTitle === "" ||
+                film.title.toLowerCase().includes(self.inputTitle.toLowerCase())) &&
+            (film.year >= self.inputYearFrom || self.inputYearFrom === "") &&
+            (film.year <= self.inputYearTo || self.inputYearTo === "")
         ) {
           if (self.inputCast === "") {
             return true;
