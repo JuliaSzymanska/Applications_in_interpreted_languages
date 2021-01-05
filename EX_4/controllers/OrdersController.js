5d55b5d7798a5c68bae82d941eda37a10c237f20
JuliaSzymanska <szymanska.julia.it@gmail.com>
2021-01-05 20:20:28 +0100

    Added log

--- EX_4/controllers/OrdersController.js
+++ EX_4/controllers/OrdersController.js
@@ -3,42 +3,66 @@ const { or } = require("sequelize");
 const db = require("../data/dataIndex");
 
 exports.findAll = (req, res) => {
-    db.sequelize.query('SELECT * FROM orders').then(orders => {
-        let newJSON = JSON.stringify(orders);
-        for (const order in orders[0]) {
-            db.sequelize.query(`SELECT * FROM products_for_orders WHERE order_id=${orders[0][order].order_id}`)
-                .then(products => {
-                    newJSON[0][order].products = products;
+    db.sequelize
+        .query("SELECT * FROM orders")
+        .then((orders) => {
+            let newJSON = orders;
+            getProductsForOrder(orders, newJSON)
+                .then((data) => {
+                    const log = require("log-to-file");
+                    log(data, "myLogs.log");
+                    res.send({ message: "Order was created successfully." });
+                })
+                .catch((e) => {
+                    res.status(400).send({
+                        message: "Error adding products to order",
+                    });
                 });
-        }
-        res.send(orders);
-    }).catch(err => {
-        res.status(400).send({
-            message: err.message || "Error ocurred while getting all the Orders."
-        });
-    });
 
+            res.send(newJSON);
+        })
+        .catch((err) => {
+            res.status(400).send({
+                message: err.message || "Error ocurred while getting all the Orders.",
+            });
+        });
 };
+
+function getProductsForOrder(orders, newJSON) {
+    const promises = [];
+
+    for (const order in orders) {
+        promises.push(
+            db.sequelize
+                .query(
+                    `SELECT * FROM products_for_orders WHERE order_id=${orders[0][order].order_id}`
+                )
+                .then((products) => {
+                    newJSON[0][order].products = products;
+                })
+        );
+    }
+    return Promise.all(promises);
+}
 
 exports.findById = (req, res) => {
     const id = req.params.id;
 
     if (!id || id === "") {
         res.status(400).send({
-            message: "ID can not be empty!"
+            message: "ID can not be empty!",
         });
         return;
     }
 
-    db.sequelize.query(`
-                            SELECT * FROM orders o where o.order_id = $ { id }
-                            `)
-        .then(data => {
+    db.sequelize
+        .query(`SELECT * FROM orders o where o.order_id = $ { id }`)
+        .then((data) => {
             res.send(data);
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(400).send({
-                message: "Error retrieving Orders with id=" + id
+                message: "Error retrieving Orders with id=" + id,
             });
         });
 };
@@ -48,21 +72,19 @@ exports.findByName = (req, res) => {
 
     if (!buyer_login || buyer_login === "") {
         res.status(400).send({
-            message: "Buyer login can not be empty!"
+            message: "Buyer login can not be empty!",
         });
         return;
     }
 
-
-    db.sequelize.query(`
-                            SELECT * FROM orders o where o.buyer_login = '${buyer_login}'
-                            `)
-        .then(data => {
+    db.sequelize
+        .query(`SELECT * FROM orders o where o.buyer_login = '${buyer_login}'`)
+        .then((data) => {
             res.send(data);
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(400).send({
-                message: err.message || "Error retrieving order by buyer login"
+                message: err.message || "Error retrieving order by buyer login",
             });
         });
 };
@@ -70,15 +92,16 @@ exports.findByName = (req, res) => {
 exports.findByStatus = (req, res) => {
     const status = req.params.status;
 
-    db.sequelize.query(`
-                            SELECT * FROM orders o where o.status_id = '${status}'
-                            `)
-        .then(data => {
+    db.sequelize
+        .query(`SELECT * FROM orders o where o.status_id = '${status}'`)
+        .then((data) => {
             res.send(data);
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(400).send({
-                message: err.message || "Some error occurred while retrieving order by status."
+                message:
+                    err.message ||
+                    "Some error occurred while retrieving order by status.",
             });
         });
 };
@@ -88,63 +111,70 @@ exports.update = (req, res) => {
 
     if (!id || id === "") {
         res.status(400).send({
-            message: "ID can not be empty!"
+            message: "ID can not be empty!",
         });
         return;
     }
 
     if (!req.params.status || req.params.status === "") {
         res.status(400).send({
-            message: "Status can not be empty!"
+            message: "Status can not be empty!",
         });
         return;
     }
 
-    db.sequelize.query(`
-                            SELECT s.status_id from orders s where s.order_id = $ { id }
-                            `).then(
-        value => {
+    db.sequelize
+        .query(`SELECT s.status_id from orders s where s.order_id = $ { id }`)
+        .then((value) => {
             value = JSON.stringify(value[0][0].status_id);
             // const log = require('log-to-file');
             // log(value, "myLogs.log");
             if (value == 2 || value >= req.params.status) {
                 res.status(400).send({
-                    message: "Unable to set this status"
+                    message: "Unable to set this status",
                 });
             } else {
-                db.sequelize.query(`
-                            UPDATE orders SET status_id = $ { req.params.status }
-                            where order_id = $ { id }
-                            `)
-                    .then(num => {
+                db.sequelize
+                    .query(`UPDATE orders SET status_id = $ { req.params.status }
+                            where order_id = $ { id }`)
+                    .then((num) => {
                         if (num[1] == 1) {
                             res.send({
-                                message: "Orders was updated successfully."
+                                message: "Orders was updated successfully.",
                             });
                         } else {
                             res.send({
                                 message: `
-                            Cannot update Orders with id = $ { id }.Maybe Orders was not found or req.body is empty!`
+                            Cannot update Orders with id = $ { id }.Maybe Orders was not found or req.body is empty!`,
                             });
                         }
                     })
-                    .catch(err => {
+                    .catch((err) => {
                         res.status(400).send({
-                            message: "Error updating Orders with id=" + id
+                            message: "Error updating Orders with id=" + id,
                         });
                     });
             }
-        }
-    );
+        });
 };
-
 
 //TODO: dodawanie produktow zamowionych
 exports.create = (req, res) => {
-    if (!req.body.buyer_login || !req.body.buyer_email || !req.body.status_id || !req.body.buyer_phone_number || !req.body.approval_date || !req.body.products ||
-        req.body.buyer_login == "" || req.body.buyer_email == "" || req.body.status_id == "" || req.body.buyer_phone_number == "" || req.body.buyer_phone_number == "") {
+    if (
+        !req.body.buyer_login ||
+        !req.body.buyer_email ||
+        !req.body.status_id ||
+        !req.body.buyer_phone_number ||
+        !req.body.approval_date ||
+        !req.body.products ||
+        req.body.buyer_login == "" ||
+        req.body.buyer_email == "" ||
+        req.body.status_id == "" ||
+        req.body.buyer_phone_number == "" ||
+        req.body.buyer_phone_number == ""
+    ) {
         res.status(400).send({
-            message: "Contents can not be empty!"
+            message: "Contents can not be empty!",
         });
         return;
     }
@@ -157,27 +187,30 @@ exports.create = (req, res) => {
         buyer_email: req.body.buyer_email,
         buyer_phone_number: req.body.buyer_phone_number,
         status_id: req.body.status_id,
-        products: req.body.products
+        products: req.body.products,
     };
 
     for (const i in order.products) {
         if (order.products[i].numberOfItems <= 0) {
             res.status(400).send({
-                message: "Product amount must be greater than 0"
+                message: "Product amount must be greater than 0",
             });
         }
     }
 
-    db.sequelize.query(`INSERT INTO orders 
+    db.sequelize
+        .query(
+            `INSERT INTO orders 
     (approval_date, status_id, buyer_login, buyer_email, buyer_phone_number)
     values( '${order.approval_date}', 
              ${order.status_id}, 
             '${order.buyer_login}', 
             '${order.buyer_email}', 
-            '${order.buyer_phone_number}')`)
-        .then(data => {
-            db.sequelize.query(`SELECT @@IDENTITY`).then(data2 => {
-                const log = require('log-to-file');
+            '${order.buyer_phone_number}')`
+        )
+        .then((data) => {
+            db.sequelize.query(`SELECT @@IDENTITY`).then((data2) => {
+                const log = require("log-to-file");
                 let insertedOrderId = JSON.stringify(data2[0][0][""]);
                 // log(insertedOrderId, "myLogs.log");
 
@@ -187,15 +220,14 @@ exports.create = (req, res) => {
                     })
                     .catch((e) => {
                         res.status(400).send({
-                            message: "Error adding products to order"
+                            message: "Error adding products to order",
                         });
                     });
-
-
             });
-        }).catch((e) => {
+        })
+        .catch((e) => {
             res.status(400).send({
-                message: "Error creating order"
+                message: "Error creating order",
             });
         });
 };
@@ -204,10 +236,13 @@ function productsToInsertLoop(insertedOrderId, order) {
     const promises = [];
 
     for (const i in order.products) {
-        promises.push(db.sequelize.query(`INSERT INTO products_for_orders (order_id, product_id, number_of_items)
+        promises.push(
+            db.sequelize
+                .query(`INSERT INTO products_for_orders (order_id, product_id, number_of_items)
         values( ${insertedOrderId}, 
                 ${order.products[i].productId},
-                ${order.products[i].numberOfItems})`));
+                ${order.products[i].numberOfItems})`)
+        );
     }
 
     return Promise.all(promises);
