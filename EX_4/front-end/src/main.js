440231cc45d03384eca6ff73feca09cc17fc5fbf
JuliaSzymanska <szymanska.julia.it@gmail.com>
2021-01-05 23:23:04 +0100

    Added fornt end routing

--- EX_4/front-end/src/main.js
+++ EX_4/front-end/src/main.js
@@ -1,9 +1,14 @@
 /*jshint esversion: 6 */
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
+
+createApp(App).use(router).mount('#app');
+export default router;
--- EX_4/front-end/src/views/Home.vue
+++ EX_4/front-end/src/views/Home.vue
@@ -1,1 +1,37 @@
 /*jshint esversion: 6 */
+<template>
+  <div class=".container-fluid">
+    <p>HEJKA NAKLEJKA</p>
+  </div>
+</template>
+
+<script>
+// import ChatsList from "../components/ChatsList";
+export default {
+  name: "Home",
+  //   components: {
+  //     ChatsList
+  //   },
+  data() {},
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+  width: 100%;
+  font-size: 16px;
+}
+body {
+  background: #00abb7;
+}
+.container-fluid * {
+  width: 100%;
+  table-layout: fixed;
+}
+</style>
